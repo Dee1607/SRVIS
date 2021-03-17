e136ce8ac0fb158910932bce9f93b178d9aa29a0
Tanuj Sobti <tn308788@dal.ca>
2021-03-17 16:49:45 -0300

    Java project added

---
+++ SRVIS/src/test/java/HelloTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloTest {
+
+    @Test
+    void addTest() {
+
+        Hello h=new Hello();
+        int c=h.add(1,2);
+        assertEquals(3,c);
+    }
+}
