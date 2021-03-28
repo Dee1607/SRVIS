2735d111cf884a52dea2f37ec2fdf33d4f2ca41d
Deep_Prashantkumar_Patel <dp889845@dal.ca>
2021-03-28 09:59:09 -0300

    Change enum indexes

--- SRVIS/src/main/java/enums/EnumServiceCategory.java
+++ SRVIS/src/main/java/enums/EnumServiceCategory.java
@@ -1,9 +1,15 @@
 package enums;
 
 public enum EnumServiceCategory {
-    Electrician,
-    Plumber,
-    Carpenter,
-    Painter,
-    Cleaner
+    Electrician(1),
+    Plumber(2),
+    Carpenter(3),
+    Painter(4),
+    Cleaner(5);
+
+    int categoryNumber = 0;
+
+    EnumServiceCategory(int serviceNumber) {
+        this.categoryNumber = serviceNumber;
+    }
 }
