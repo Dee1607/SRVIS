f64202e824fba86700296318a2457f1db2124981
Deep_Prashantkumar_Patel <dp889845@dal.ca>
2021-03-28 10:36:51 -0300

    Refactoring

--- SRVIS/src/main/java/SearchServiceCategory/GenerateDataToDisplay.java
+++ SRVIS/src/main/java/SearchServiceCategory/GenerateDataToDisplay.java
@@ -22,4 +22,6 @@ public class GenerateDataToDisplay {
 
         return mapSearchCategories;
     }
+
+
 }
--- SRVIS/src/main/java/mainUI.java
+++ SRVIS/src/main/java/mainUI.java
@@ -1,3 +1,5 @@
+import login.LoginService;
+import presantationlayer.DisplayToGetUserChoice;
 import presentationlayer.LoginUI;
 
 public class mainUI
@@ -5,11 +7,22 @@ public class mainUI
     public static void main(String[] args)
     {
         LoginUI log = null;
+        LoginService login = null;
+        DisplayToGetUserChoice objGetData = null;
         try
         {
             log = new LoginUI();
 
-            log.showLoginScreen();
+//            log.showLoginScreen();
+
+            objGetData = new DisplayToGetUserChoice();
+            String username = objGetData.displayMessageGetStringChoiceFromUser("Enter your Username: ");
+            String password = objGetData.displayMessageGetStringChoiceFromUser("Enter your password: ");
+            String type = objGetData.displayMessageGetStringChoiceFromUser("Login as Customer/Service provider: ");
+
+            login = new LoginService();
+            login.loginUser(username, password,type);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
