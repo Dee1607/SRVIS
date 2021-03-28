d7fe9eca7206fa1ca571f35fc1ac96b03ab6bf2c
Deep_Prashantkumar_Patel <dp889845@dal.ca>
2021-03-28 09:58:37 -0300

    Created block for next feature development

--- SRVIS/src/main/java/bookingServiceProvider/BookServiceProvider.java
+++ SRVIS/src/main/java/bookingServiceProvider/BookServiceProvider.java
@@ -1,7 +1,18 @@
 package bookingServiceProvider;
 
+import database.ConnectionToDB;
+import presantationlayer.DisplayToGetUserChoice;
+
+import java.util.Map;
+
 public class BookServiceProvider {
-    public void bookServiceProvider(){
+    DisplayToGetUserChoice objToDisplayData = null;
+    ConnectionToDB objDatabaseConnection = null;
+
+    public void bookServiceProvider(Map<String,Map<String,String>> customerSessionDetails, String selectedServiceProviderID){
+        objToDisplayData = new DisplayToGetUserChoice();
+
+        String descriptionOfWork = objToDisplayData.displayMessageGetStringChoiceFromUser("Give some brief information on the work needs to be done: ");
 
     }
 }
