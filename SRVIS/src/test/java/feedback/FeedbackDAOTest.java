0da1dfc4cbd7c1dd5047fc33c55d04831a3a879c
Philemon Lee <philemon.lee@dal.ca>
2021-03-31 16:40:36 -0300

    Created FeedbackDAOTest

---
+++ SRVIS/src/test/java/feedback/FeedbackDAOTest.java
@@ -0,0 +1,26 @@
+package feedback;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FeedbackDAOTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void write() {
+    }
+}
