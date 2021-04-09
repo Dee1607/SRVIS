be5239cb313e94d6ac226fc8625969ca72df0c79
Akash Madan <ak556738@dal.ca>
2021-04-08 23:09:53 -0300

    encryption

---
+++ SRVIS/src/main/java/encryptionservice/IEncryptionService.java
@@ -0,0 +1,7 @@
+package encryptionservice;
+
+import java.util.ArrayList;
+
+public interface IEncryptionService {
+    public ArrayList<String> encryptDataForSecurity(String stringToEncrypt);
+}
--- SRVIS/src/main/java/presentationlayer/DisplayRegistrationPageUI.java
+++ SRVIS/src/main/java/presentationlayer/DisplayRegistrationPageUI.java
@@ -1,5 +1,6 @@
 package presentationlayer;
 
+import encryptionservice.IEncryptionService;
 import registration.IValidation;
 import registration.Validation;
 import java.util.ArrayList;
@@ -9,6 +10,7 @@ import java.util.Scanner;
 public class DisplayRegistrationPageUI {
     IValidation validate = null;
     DisplayServiceCategoriesUI displayData;
+    IEncryptionService encryptSecurity = null;
 
     public DisplayRegistrationPageUI() {
         validate = new Validation();
@@ -22,6 +24,11 @@ public class DisplayRegistrationPageUI {
             Scanner sc = new Scanner(System.in);
             String value = sc.nextLine();
             boolean validation = validate.isValidString(pattern, value);
+
+            if(methodName=="new password(only characters and numbers are allowed)"){
+                ArrayList<String> getEncryptedValue = encryptSecurity.encryptDataForSecurity(value);
+                value = getEncryptedValue.get(0);
+            }
 
             if (validation == true) {
                 result.add(validation + "-" + value);
